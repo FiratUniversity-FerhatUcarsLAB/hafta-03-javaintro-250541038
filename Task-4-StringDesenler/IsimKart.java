b7f53c731ef33b2a02d5f7700368d71097b35cb6
250541038 <250541038@firat.edu.tr>
2025-10-22 12:14:58 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/IsimKart.java
@@ -0,0 +1,19 @@
+public class IsimKart {
+    public static void main(String[] args) {
+        String ad = " Muhammed Metin ";
+        String soyad = " Kızıl ";
+        String ogrNo = " 250541038 ";
+        String bolum = " Yazilim Muh .";
+
+        System.out.println(" =================== ");
+        System.out.println(" OGRENCI BILGILERI ");
+        System.out.println(" =================== ");
+        System.out.println("Ad : " + ad );
+        System.out.println("Soyad : " + soyad );
+        System.out.println("No : " + ogrNo );
+        System.out.println("Bolum : " + bolum );
+        System.out.println(" =================== ");
+        }
+}
+
+
