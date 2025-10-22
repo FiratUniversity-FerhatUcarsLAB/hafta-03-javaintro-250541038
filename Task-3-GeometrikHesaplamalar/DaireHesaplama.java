7a9adecd0ca5a9c4cc5d2b857ef1ab06a14e854a
250541038 <250541038@firat.edu.tr>
2025-10-22 12:04:05 +0300

    Add files via upload

---
+++ Task-3-GeometrikHesaplamalar/DaireHesaplama.java
@@ -0,0 +1,17 @@
+public class DaireHesaplama {
+    public static void main(String[] args) {
+    double r = 5.5;
+    double pi = 3.14159;
+
+    //Alan Hesapla
+    double alan = pi * r * r;
+
+    //Cevre Hesapla
+    double cevre = pi * 2 * r;
+
+    //Sonuc Yazdır
+    System.out.println(" Yaricap : " + r);
+    System.out.println(" Alan : " + alan);
+    System.out.println(" Cevre : " + cevre);
+    }
+}
