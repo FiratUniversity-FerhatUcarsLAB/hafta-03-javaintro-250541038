deacb10471b6873243562669648a1d3e3c6d4ef2
250541038 <250541038@firat.edu.tr>
2025-10-22 12:09:46 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/ASCIIArt.java
@@ -0,0 +1,13 @@
+public class ASCIIArt {
+    public static void main(String[] args) {
+        System.out.println("M harfi ASCII Art gösterimi:");
+        System.out.println(" "); // Boşluk bırakalım
+
+        // M harfini oluşturan satırlar
+        System.out.println("M       M");
+        System.out.println("M M   M M");
+        System.out.println("M   M   M");
+        System.out.println("M       M");
+        System.out.println("M       M");
+    }
+}
