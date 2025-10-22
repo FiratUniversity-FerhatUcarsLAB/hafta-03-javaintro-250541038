f41bf783a90c70df8b922e8aeb0bd05b192688ab
250541038 <250541038@firat.edu.tr>
2025-10-22 11:57:13 +0300

    Add files via upload

---
+++ Task-2-Matematikselislemler/IkiIslemSonuc.java
@@ -0,0 +1,12 @@
+public class IkiIslemSonuc {
+    public static void main(String[] args) {
+        //Pay Hesapla
+        Double pay = (9.5*4.5) - (2.5*3);
+        //Payda Heapla
+        Double payda = (45.5 - 3.5);
+        //Bolme Yap
+        Double sonuc = pay/payda;
+
+        System.out.printf("Sonuc: " + sonuc);
+    }
+}
