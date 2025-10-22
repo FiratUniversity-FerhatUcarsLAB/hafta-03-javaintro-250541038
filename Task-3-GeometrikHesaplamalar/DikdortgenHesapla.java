ac8ab7cebc057e316bb08df5a99be46c60a64c78
250541038 <250541038@firat.edu.tr>
2025-10-22 12:06:13 +0300

    Add files via upload

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesapla.java
@@ -0,0 +1,19 @@
+public class DikdortgenHesapla {
+    public static void main(String[] args) {
+
+        double en = 4.5;
+        double boy = 7.9;
+
+        //Alan Hesapla
+        double alan = en * boy;
+
+        //Cevre Hesapla
+        double cevre = (en * 2) + (boy * 2);
+
+        //Sonucları Yazdır
+        System.out.println("Alan: " + alan);
+        System.out.println("CEVRE: " + cevre);
+
+    }
+}
+
