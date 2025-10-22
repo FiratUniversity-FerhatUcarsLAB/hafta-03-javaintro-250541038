88823b997ea75f0782b1866c8fa215c736318544
250541038 <250541038@firat.edu.tr>
2025-10-22 12:21:49 +0300

    Add files via upload

---
+++ Task-5-TabloDesenleri/CarpimTablasu.java
@@ -0,0 +1,37 @@
+public class CarpimTablasu {
+    public static void main(String[] args) {
+        System.out.println(" a*n ");
+        int a = 2;
+        int n1  = 1;
+        int n2 = 2;
+        int n3 = 3;
+        int n4 = 4;
+        int n5 = 5;
+        int n6 = 6;
+        int n7 = 7;
+        int n8 = 8;
+        int n9 = 9;
+
+        int hesapla1 = a*n1;
+        int hesapla2 = a*n2;
+        int hesapla3 = a*n3;
+        int hesapla4 = a*n4;
+        int hesapla5 = a*n5;
+        int hesapla6 = a*n6;
+        int hesapla7 = a*n7;
+        int hesapla8 = a*n8;
+        int hesapla9 = a*n9;
+
+        System.out.println("a*n1 ="  + hesapla1);
+        System.out.println("a*n2 ="  + hesapla2);
+        System.out.println("a*n3 ="  + hesapla3);
+        System.out.println("a*n4 ="  + hesapla4);
+        System.out.println("a*n5 ="  + hesapla5);
+        System.out.println("a*n6 ="  + hesapla6);
+        System.out.println("a*n7 ="  + hesapla7);
+        System.out.println("a*n8 ="  + hesapla8);
+        System.out.println("a*n9 ="  + hesapla9);
+
+
+    }
+}
